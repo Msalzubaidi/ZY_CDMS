8ab6aeea90cff8a4361487425b1f77c4eb0fee59
Mohammad Alzubaidi <dev.mzubaidi@gmail.com>
2020-12-29 00:46:45 +0200

    00200

--- ZY_CDMS/Forms/frmLogin.Designer.cs
+++ ZY_CDMS/Forms/frmLogin.Designer.cs
@@ -35,48 +35,52 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.ID = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
             this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
-            this.credInfo = new System.Windows.Forms.Label();
             this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.credInfo = new System.Windows.Forms.Label();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_username
             // 
             this.txt_username.Enabled = false;
-            this.txt_username.Location = new System.Drawing.Point(190, 147);
+            this.txt_username.Location = new System.Drawing.Point(90, 135);
             this.txt_username.Name = "txt_username";
-            this.txt_username.Size = new System.Drawing.Size(205, 20);
+            this.txt_username.Size = new System.Drawing.Size(179, 20);
             this.txt_username.TabIndex = 1;
             // 
             // txt_password
             // 
-            this.txt_password.Location = new System.Drawing.Point(190, 174);
+            this.txt_password.Location = new System.Drawing.Point(90, 161);
             this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(205, 20);
+            this.txt_password.Size = new System.Drawing.Size(179, 20);
             this.txt_password.TabIndex = 2;
             this.txt_password.UseSystemPasswordChar = true;
             // 
             // txt_userid
             // 
-            this.txt_userid.Location = new System.Drawing.Point(190, 121);
+            this.txt_userid.Location = new System.Drawing.Point(90, 109);
             this.txt_userid.Name = "txt_userid";
-            this.txt_userid.Size = new System.Drawing.Size(205, 20);
+            this.txt_userid.Size = new System.Drawing.Size(179, 20);
             this.txt_userid.TabIndex = 0;
             this.txt_userid.Leave += new System.EventHandler(this.simpleButton3_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(153, 147);
+            this.pictureBox1.Location = new System.Drawing.Point(258, 279);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(35, 20);
+            this.pictureBox1.Size = new System.Drawing.Size(36, 32);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -84,9 +88,9 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(153, 121);
+            this.pictureBox2.Location = new System.Drawing.Point(220, 279);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(35, 20);
+            this.pictureBox2.Size = new System.Drawing.Size(36, 32);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
@@ -94,86 +98,114 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(153, 174);
+            this.pictureBox3.Location = new System.Drawing.Point(296, 279);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(35, 20);
+            this.pictureBox3.Size = new System.Drawing.Size(36, 32);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 5;
             this.pictureBox3.TabStop = false;
             // 
-            // ID
-            // 
-            this.ID.AutoSize = true;
-            this.ID.BackColor = System.Drawing.Color.Transparent;
-            this.ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ID.Location = new System.Drawing.Point(86, 124);
-            this.ID.Name = "ID";
-            this.ID.Size = new System.Drawing.Size(20, 13);
-            this.ID.TabIndex = 6;
-            this.ID.Text = "ID";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(86, 150);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Username";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(86, 177);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Password";
-            // 
             // simpleButton1
             // 
-            this.simpleButton1.Location = new System.Drawing.Point(78, 234);
+            this.simpleButton1.Location = new System.Drawing.Point(56, 213);
             this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(129, 23);
+            this.simpleButton1.Size = new System.Drawing.Size(106, 36);
             this.simpleButton1.TabIndex = 3;
             this.simpleButton1.Text = "Login";
             this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
             // 
             // simpleButton2
             // 
-            this.simpleButton2.Location = new System.Drawing.Point(327, 234);
+            this.simpleButton2.Location = new System.Drawing.Point(174, 213);
             this.simpleButton2.Name = "simpleButton2";
-            this.simpleButton2.Size = new System.Drawing.Size(129, 23);
+            this.simpleButton2.Size = new System.Drawing.Size(106, 36);
             this.simpleButton2.TabIndex = 4;
             this.simpleButton2.Text = "Exit";
             this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Location = new System.Drawing.Point(641, 282);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(106, 23);
+            this.simpleButton3.TabIndex = 10;
+            this.simpleButton3.Text = "ViewUserInfo";
+            this.simpleButton3.Visible = false;
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
             // 
             // credInfo
             // 
             this.credInfo.AutoSize = true;
             this.credInfo.BackColor = System.Drawing.Color.Transparent;
             this.credInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.credInfo.Location = new System.Drawing.Point(183, 60);
+            this.credInfo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.credInfo.Location = new System.Drawing.Point(6, 9);
             this.credInfo.Name = "credInfo";
-            this.credInfo.Size = new System.Drawing.Size(267, 52);
+            this.credInfo.Size = new System.Drawing.Size(65, 13);
             this.credInfo.TabIndex = 9;
-            this.credInfo.Text = "Development Mode \r\nUserID : 1\r\nUsername : admin\r\nPassword : DateOfDay Format (ddm" +
-    "myyyy : 01122020 )\r\n";
+            this.credInfo.Text = "Admin Login";
             this.credInfo.Click += new System.EventHandler(this.credInfo_Click);
             // 
-            // simpleButton3
+            // labelControl1
             // 
-            this.simpleButton3.Location = new System.Drawing.Point(455, 291);
-            this.simpleButton3.Name = "simpleButton3";
-            this.simpleButton3.Size = new System.Drawing.Size(75, 23);
-            this.simpleButton3.TabIndex = 10;
-            this.simpleButton3.Text = "ViewUserInfo";
-            this.simpleButton3.Visible = false;
-            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(26, 112);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(42, 13);
+            this.labelControl1.TabIndex = 11;
+            this.labelControl1.Text = "User ID";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(26, 138);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(61, 13);
+            this.labelControl2.TabIndex = 12;
+            this.labelControl2.Text = "User Name";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(26, 164);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(54, 13);
+            this.labelControl3.TabIndex = 13;
+            this.labelControl3.Text = "Password";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(182, 279);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(36, 32);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 15;
+            this.pictureBox4.TabStop = false;
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl4.Appearance.Options.UseFont = true;
+            this.labelControl4.Location = new System.Drawing.Point(144, 52);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(59, 23);
+            this.labelControl4.TabIndex = 16;
+            this.labelControl4.Text = "Log in";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 288);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 12);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "ZY Technologies Solutions © 2021";
             // 
             // frmLogin
             // 
@@ -181,14 +213,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(533, 317);
+            this.ClientSize = new System.Drawing.Size(759, 317);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.simpleButton3);
             this.Controls.Add(this.credInfo);
             this.Controls.Add(this.simpleButton2);
             this.Controls.Add(this.simpleButton1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.ID);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -207,6 +242,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,12 +256,15 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Label ID;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
         private DevExpress.XtraEditors.SimpleButton simpleButton2;
-        private System.Windows.Forms.Label credInfo;
         private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private System.Windows.Forms.Label credInfo;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private System.Windows.Forms.Label label1;
     }
 }
